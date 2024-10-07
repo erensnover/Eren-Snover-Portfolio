e7082491b0869a40494bbca1499c269b46871677
Eren Snover <erensrocks@gmail.com>
2024-10-07 19:38:50 -0400

    Update sql_query_example.py

--- sql_query_example.py
+++ sql_query_example.py
@@ -1,8 +1,8 @@
 '''
-I utilized DuckDB in this file, a library which allows you to seemlessly integrate Sql code directly into a Python file.
-I originally wrote this file without using DuckDB and joined my datasets in python. This process however took several hours to complete,
-and knowing that this file would likely need to be run more than once, I decided to instead run the join in Sql.
-This allows me to conditionally the large data sets in seconds rather than hours.
+This is a file I used for a project where I had to combine informationa about people from two different data sets. These data sets were organized by peoples names,
+however their names were often spelt differently between each set (Ex: "John Smith" vs "John Patrick Smith Jr."). I found the program correctly organized data most
+efficiently when it would allow for names which were similar but not exact, like the previous example. This process would be impossible using Pandas to merge the 
+two data sets. However, using SQL's JOIN functionality, I was able to easy combine these data sets using such conditions.
 '''
 import pandas as pd
 import duckdb
@@ -17,31 +17,31 @@ def listFormat(x):
         return tuple([x])
 
 
-df1=pd.read_excel(r"file path",0)
-df2=pd.read_excel(r"file path",1)
-df1=df1.reset_index()
-df2=df2.reset_index()
+dataframe1=pd.read_excel(r"C:\Users\erens\Desktop\Eren-Snover-Portfolio\dataset1",0)
+dataframe2=pd.read_excel(r"C:\Users\erens\Desktop\Eren-Snover-Portfolio\dataset2",1)
+dataframe1=dataframe1.reset_index()
+dataframe2=dataframe2.reset_index()
 
-df1 = df1[['index','Uid','Taxpayer','AppName','SpouseName','Address']]
-df1[['AppName','SpouseName']] = df1[['AppName','SpouseName']].astype(str).apply(lambda x: [[i for i in j.upper().split() if '/' not in i] for j in x]).apply(lambda x: [listFormat(tuple(i)) for i in x])
-df2[['Grantor','Grantee']] = df2[['Grantor','Grantee']].astype(str).apply(lambda x: [tuple(i.upper().split()) for i in x])
+dataframe1 = dataframe1[['index','id','Taxpayer','AppName','SpouseName','Address']]
+dataframe1[['AppName','SpouseName']] = dataframe1[['AppName','SpouseName']].astype(str).apply(lambda x: [[i for i in j.upper().split() if '/' not in i] for j in x]).apply(lambda x: [listFormat(tuple(i)) for i in x])
+dataframe2[['Grantor','Grantee']] = dataframe2[['Grantor','Grantee']].astype(str).apply(lambda x: [tuple(i.upper().split()) for i in x])
 
 sql='''
 SELECT *
-FROM df1
-LEFT JOIN df2
-ON (((((cast(df2.Grantor AS VARCHAR[]) @> cast(df1.AppName[1] AS VARCHAR[])) OR (cast(df2.Grantor AS VARCHAR[]) <@ cast(df1.AppName[1] AS VARCHAR[]))) AND len(df1.AppName[1])>1) OR
-    (((cast(df2.Grantor AS VARCHAR[]) @> cast(df1.AppName[2] AS VARCHAR[])) OR (cast(df2.Grantor AS VARCHAR[]) <@ cast(df1.AppName[2] AS VARCHAR[]))) AND len(df1.AppName[2])>1)) AND list_contains(df2.Grantor,'EST')) OR
-   (((((cast(df2.Grantee AS VARCHAR[]) @> cast(df1.AppName[1] AS VARCHAR[])) OR (cast(df2.Grantee AS VARCHAR[]) <@ cast(df1.AppName[1] AS VARCHAR[]))) AND len(df1.AppName[1])>1) OR
-     (((cast(df2.Grantee AS VARCHAR[]) @> cast(df1.AppName[2] AS VARCHAR[])) OR (cast(df2.Grantee AS VARCHAR[]) <@ cast(df1.AppName[2] AS VARCHAR[]))) AND len(df1.AppName[2])>1)) AND list_contains(df2.Grantee,'EST'))
+FROM dataframe1
+LEFT JOIN dataframe2
+ON (((((cast(dataframe2.Grantor AS VARCHAR[]) @> cast(dataframe1.AppName[1] AS VARCHAR[])) OR (cast(dataframe2.Grantor AS VARCHAR[]) <@ cast(dataframe1.AppName[1] AS VARCHAR[]))) AND len(dataframe1.AppName[1])>1) OR
+    (((cast(dataframe2.Grantor AS VARCHAR[]) @> cast(dataframe1.AppName[2] AS VARCHAR[])) OR (cast(dataframe2.Grantor AS VARCHAR[]) <@ cast(dataframe1.AppName[2] AS VARCHAR[]))) AND len(dataframe1.AppName[2])>1)) AND list_contains(dataframe2.Grantor,'EST')) OR
+   (((((cast(dataframe2.Grantee AS VARCHAR[]) @> cast(dataframe1.AppName[1] AS VARCHAR[])) OR (cast(dataframe2.Grantee AS VARCHAR[]) <@ cast(dataframe1.AppName[1] AS VARCHAR[]))) AND len(dataframe1.AppName[1])>1) OR
+     (((cast(dataframe2.Grantee AS VARCHAR[]) @> cast(dataframe1.AppName[2] AS VARCHAR[])) OR (cast(dataframe2.Grantee AS VARCHAR[]) <@ cast(dataframe1.AppName[2] AS VARCHAR[]))) AND len(dataframe1.AppName[2])>1)) AND list_contains(dataframe2.Grantee,'EST'))
 '''
 
 sql2='''
 SELECT *
 FROM tableJoin
-LEFT JOIN df2
-ON ((((cast(df2.Grantor AS VARCHAR[]) @> cast(tableJoin.SpouseName[1] AS VARCHAR[])) OR (cast(df2.Grantor AS VARCHAR[]) <@ cast(tableJoin.SpouseName[1] AS VARCHAR[]))) AND len(tableJoin.SpouseName[1])>1) AND list_contains(df2.Grantor,'EST')) OR
-   ((((cast(df2.Grantee AS VARCHAR[]) @> cast(tableJoin.SpouseName[1] AS VARCHAR[])) OR (cast(df2.Grantee AS VARCHAR[]) <@ cast(tableJoin.SpouseName[1] AS VARCHAR[]))) AND len(tableJoin.SpouseName[1])>1) AND list_contains(df2.Grantee,'EST'))
+LEFT JOIN dataframe2
+ON ((((cast(dataframe2.Grantor AS VARCHAR[]) @> cast(tableJoin.SpouseName[1] AS VARCHAR[])) OR (cast(dataframe2.Grantor AS VARCHAR[]) <@ cast(tableJoin.SpouseName[1] AS VARCHAR[]))) AND len(tableJoin.SpouseName[1])>1) AND list_contains(dataframe2.Grantor,'EST')) OR
+   ((((cast(dataframe2.Grantee AS VARCHAR[]) @> cast(tableJoin.SpouseName[1] AS VARCHAR[])) OR (cast(dataframe2.Grantee AS VARCHAR[]) <@ cast(tableJoin.SpouseName[1] AS VARCHAR[]))) AND len(tableJoin.SpouseName[1])>1) AND list_contains(dataframe2.Grantee,'EST'))
 '''
 
 tableJoin = duckdb.sql(sql).df()
